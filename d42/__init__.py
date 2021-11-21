030a66ddf37b4f331f5eec4d8273efd4251dade2
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-11-21 19:43:40 +0300

    add impl

---
+++ d42/__init__.py
@@ -0,0 +1,8 @@
+import revolt  # noqa: F401
+from blahblah import fake
+from district42 import from_native, optional, register_type, schema
+from valera import validate, validate_or_fail
+
+__all__ = ("schema", "optional", "from_native", "register_type",
+           "fake", "validate", "validate_or_fail",)
+__version__ = "0.0.1"
