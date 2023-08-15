c2766a4d9f6b3dc1c6244328eacb46ace222d04f
Nikita Tsvetkov <tsv1@fastmail.com>
2023-08-15 21:17:28 +0400

    bump version → 1.5.2

--- d42/__init__.py
+++ d42/__init__.py
@@ -9,7 +9,7 @@ except AttributeError:
     from typing import Any
 
     def make_required(schema: Any, keys: Any = None) -> Any:
-        raise ValueError("make_required is not available in district42 < 1.5.1")
+        raise ValueError("make_required is not available in district42 < 1.5.2")
 
 schema = district42.schema
 optional = district42.optional
@@ -30,4 +30,4 @@ __all__ = (
     "validate", "validate_or_fail", "ValidationException",
     "substitute",
 )
-__version__ = "1.5.1"
+__version__ = "1.5.2"
