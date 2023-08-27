06b7eebf2ce68ca7ac1cacd37ae34e5d2b6aebf3
Nikitа Tsvetkоv <tsv1@fastmail.com>
2023-08-27 16:36:04 +0400

    add custom type (#2)

---
+++ d42/custom_type/utils.py
@@ -0,0 +1,4 @@
+from district42.errors import make_already_declared_error, make_invalid_type_error
+from revolt.errors import make_substitution_error
+
+__all__ = ("make_invalid_type_error", "make_already_declared_error", "make_substitution_error",)
