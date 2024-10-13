e6d4bf85b78cb2a59aede845599d7bfb6735cbc3
Nikita Tsvetkov <tsv1@fastmail.com>
2024-10-13 17:42:16 +0400

    fix command

--- d42/_main.py
+++ d42/_main.py
@@ -5,17 +5,23 @@ from d42.migration.migrate_v1_to_v2 import migrate_v1_to_v2
 
 
 def run() -> None:
-    parser = argparse.ArgumentParser("d42", description="Migrate schemas from v1 to v2")
-    parser.add_argument("schema_directory", type=str,
-                        help="Path to the directory containing schema files")
+    parser = argparse.ArgumentParser("d42")
+
+    subparsers = parser.add_subparsers(dest="command", help="Subcommands")
+    v1_to_v2_parser = subparsers.add_parser("v1-to-v2", help="Migrate schemas from v1 to v2")
+    v1_to_v2_parser.add_argument("schema_directory", type=str,
+                                 help="Path to the directory containing schema files")
 
     args = parser.parse_args()
-    if not os.path.isdir(args.schema_directory):
-        print(f"Error: The path '{args.schema_directory}' is not a valid directory")
-        return
+    if args.command == "v1-to-v2":
+        if not os.path.isdir(args.schema_directory):
+            print(f"Error: The path '{args.schema_directory}' is not a valid directory")
+            return
 
-    migrate_v1_to_v2(args.schema_directory)
-    print(f"Migration completed for schemas in '{args.schema_directory}'")
+        migrate_v1_to_v2(args.schema_directory)
+        print(f"Migration completed for schemas in '{args.schema_directory}'")
+    else:
+        parser.print_help()
 
 
 if __name__ == "__main__":
