72a4ece5eb644ff980d5f000e3f5994dfce1c2c6
Nikita Tsvetkov <tsv1@fastmail.com>
2024-10-13 15:02:49 +0400

    add test

--- tests/declaration/schema/test_register_visitor.py
+++ tests/declaration/schema/test_register_visitor.py
@@ -1,8 +1,6 @@
 from baby_steps import given, then, when
-from pytest import raises
 
-from d42.declaration import Props, Schema
-from d42.representation import Representor, represent
+from d42.declaration import Props, Schema, SchemaVisitor
 
 
 def test_register_visitor():
@@ -13,29 +11,12 @@ def test_register_visitor():
             def __accept__(self, visitor, **kwargs):
                 return visitor.visit_custom_type(self, **kwargs)
 
-        class CustomRepresentor(Representor, extend=True):
+        class CustomRepresentor(SchemaVisitor, extend=True):
             def visit_custom_type(self, schema, **kwargs):
                 return representation
 
     with when:
-        res = represent(CustomType())
+        res = CustomType().__accept__(CustomRepresentor())
 
     with then:
         assert res == representation
-
-
-def test_register_visitor_without_extend():
-    with given:
-        class AnotherCustomType(Schema[Props]):
-            def __accept__(self, visitor, **kwargs):
-                return visitor.visit_another_custom_type(self, **kwargs)
-
-        class CustomRepresentor(Representor):
-            def visit_another_custom_type(self, schema, **kwargs):
-                return "schema.another_custom_type"
-
-    with when, raises(Exception) as exception:
-        represent(AnotherCustomType())
-
-    with then:
-        assert exception.type is AttributeError
