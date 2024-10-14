993125bad8b4bac037f4ff3a952b646fbcf8b88a
Nikita Tsvetkov <tsv1@fastmail.com>
2024-10-14 21:44:36 +0400

    fix representation

--- d42/__init__.py
+++ d42/__init__.py
@@ -1,8 +1,9 @@
 from d42.declaration import optional, schema
 from d42.generation import fake
+from d42.representation import represent
 from d42.substitution import substitute
 from d42.validation import ValidationException, validate, validate_or_fail
 
 __all__ = ("schema", "optional", "fake", "validate", "validate_or_fail", "substitute",
-           "ValidationException",)
+           "ValidationException", "represent",)
 __version__ = "2.0.1"
